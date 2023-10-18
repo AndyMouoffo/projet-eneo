41218124821b3a43185f94283d00421c720dede6
paul-henryN <ngpaulhenry@gmail.com>
2023-10-18 20:53:41 +0200

    add material index view and controller logic

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Material;
 use Illuminate\Http\Request;
 
 class MaterialController extends Controller
 {
-    //
+    public function index()
+    {
+        $materials = Material::all();
+        return view('material/index', ['materials' => $materials]);
+    }
 }
